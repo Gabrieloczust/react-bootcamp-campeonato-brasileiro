26db7034d34861fc088a53ca4322b7e9fd41df1a
Gabriel Oczust <gabriel.oczust@softplan.com.br>
2022-09-20 19:14:49 -0300

    feat: buscar dados localmente se não iniciar o server

--- src/hooks/useFetchClassification/index.js
+++ src/hooks/useFetchClassification/index.js
@@ -1,47 +1,72 @@
-import { useState } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import useSWR from 'swr';
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 function useFetchClassification({ year }) {
   const [classification, setClassification] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
-  function setTeam(team, overallScore) {
-    return {
-      ...overallScore,
-      name: team,
-      total_saldo_de_gols: overallScore.total_gols_marcados - overallScore.total_gols_sofridos,
-    };
-  }
+  const setTeam = (team, overallScore) => ({
+    ...overallScore,
+    name: team,
+    total_saldo_de_gols: overallScore.total_gols_marcados - overallScore.total_gols_sofridos,
+  });
 
-  function orderClassification(a, b) {
+  const orderClassification = (a, b) => {
     const points = b.total_pontos - a.total_pontos;
     const victories = b.total_vitorias - a.total_vitorias;
     const goalBalance = b.total_saldo_de_gols - a.total_saldo_de_gols;
     const goalsScored = b.total_gols_marcados - a.total_gols_marcados;
     return points || victories || goalBalance || goalsScored;
-  }
+  };
 
-  async function prepareResponse(response) {
+  const updateClassification = useCallback(async (response) => {
     const lastRound = response.pop().partidas;
 
-    return lastRound.reduce((teams, match) => {
+    const classificationData = lastRound.reduce((teams, match) => {
       const principal = setTeam(match.mandante, match.pontuacao_geral_mandante);
       const visitor = setTeam(match.visitante, match.pontuacao_geral_visitante);
       return [...teams, principal, visitor];
     }, []).sort(orderClassification);
-  }
+
+    setClassification(classificationData);
+    setIsLoading(false);
+  }, [year]);
 
   const { error } = useSWR(`http://localhost:3001/${year}`, fetcher, {
-    onSuccess: async (response) => {
-      setClassification(await prepareResponse(response));
-    },
+    onSuccess: updateClassification,
+    revalidateOnFocus: false,
+    shouldRetryOnError: false,
   });
+
+  useEffect(() => {
+    setIsLoading(true);
+  }, [year]);
+
+  useEffect(() => {
+    if (!error) {
+      return;
+    }
+
+    try {
+      (async () => {
+        setIsLoading(true);
+        const { default: json } = await import('../../../campeonato-brasileiro.json');
+        const response = json[year];
+        updateClassification(response);
+      })();
+    } catch {
+      setIsLoading(false);
+      setIsError(true);
+    }
+  }, [error]);
 
   return {
     classification,
-    isLoading: !error && !classification.length,
-    isError: error,
+    isLoading,
+    isError,
   };
 }
 
